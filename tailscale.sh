1748305c3119cd0f99a23ca8aca048682e07b7bf
Denton Gentry <dgentry@tailscale.com>
2021-10-21 17:17:02 -0700

    Add Copyright headers.
    Signed-off-by: Denton Gentry <dgentry@tailscale.com>

--- tailscale.sh
+++ tailscale.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+# Copyright (c) 2021 Tailscale Inc & AUTHORS All rights reserved.
+# Use of this source code is governed by a BSD-style
+# license that can be found in the LICENSE file.
 
 ### BEGIN INIT INFO
 # Provides:             tailscaled
